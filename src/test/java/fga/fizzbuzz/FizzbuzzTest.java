bfd90ab52ebb67c6e1adcd48712116854d2e3ac8
Franz Amador <franzamador@gmail.com>
2022-11-06 15:25:04 -0800

    [Red] Add first test ordinaryNumberPrintsAsItself, not yet building

---
+++ src/test/java/fga/fizzbuzz/FizzbuzzTest.java
@@ -0,0 +1,12 @@
+package fga.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzbuzzTest {
+    @Test
+    void ordinaryNumberPrintsAsItself() {
+        assertEquals("2", fizzbuzz(2));
+    }
+}
