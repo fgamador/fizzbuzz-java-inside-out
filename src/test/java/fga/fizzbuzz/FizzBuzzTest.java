082fe84d742463fb6a1bf0774ec7ce420b81c758
Franz Amador <franzamador@gmail.com>
2022-11-06 17:22:15 -0800

    [Refactor] Rename FizzBuzzTest to match FizzBuzz

--- src/test/java/fga/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fga/fizzbuzz/FizzBuzzTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FizzbuzzTest {
+public class FizzBuzzTest {
     @Test
     void ordinaryNumberPrintsAsItself() {
         assertEquals("2", FizzBuzz.fizzbuzz(2));
