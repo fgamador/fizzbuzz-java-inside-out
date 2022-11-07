f8fd7e56b98ae43042890263fc3fa1f5d81c7a9a
Franz Amador <franzamador@gmail.com>
2022-11-06 17:24:47 -0800

    Finish app by calling fizzbuzz function from new main function

--- src/main/java/fga/fizzbuzz/FizzBuzz.java
+++ src/main/java/fga/fizzbuzz/FizzBuzz.java
@@ -1,6 +1,12 @@
 package fga.fizzbuzz;
 
 public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 100; num++) {
+            System.out.println(fizzbuzz(num));
+        }
+    }
+
     static String fizzbuzz(int num) {
         if (num % 15 == 0)
             return "FizzBuzz";
